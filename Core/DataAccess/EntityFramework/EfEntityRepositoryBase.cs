97c3d7d5ecf4354a4394a3b1607669b768a6f3bf
Mevlut Dizman <mdizman124@gmail.com>
2022-11-01 18:34:43 +0300

    mevlut

--- Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -1,11 +1,80 @@
-﻿using System;
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Core.DataAccess.EntityFramework
 {
     //entity tip ve context tip ver
-    class EfEntityRepositoryBase<TEntity,TContext>
+    //generic hale getirildi
+    public class EfEntityRepositoryBase<TEntity,TContext>:IEntityRepository<TEntity>
+        //TEntity ve TContext kısıtla
+        where TEntity : class,IEntity,new() 
+        where TContext : DbContext,new()
     {
+        public void Add(TEntity entity)
+        {
+            //IDisposable pattern implementation of c#
+            using (TContext context = new TContext())
+            {
+                //addedEntity : eklenen varlık 
+                //referansı yakala
+                var addedEntity = context.Entry(entity);
+                //eklenecek bir nesne olduğunu belirtiyor 
+                addedEntity.State = EntityState.Added;
+                //ekle
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            //IDisposable pattern implementation of c#
+            using (TContext context = new TContext())
+            {
+                //addedEntity : eklenen varlık 
+                //referansı yakala
+                var deletedEntity = context.Entry(entity);
+                //silinecek  bir nesne 
+                deletedEntity.State = EntityState.Deleted;
+                //sil
+                context.SaveChanges();
+            }
+        }
+        //tek data getirmek için yazıldı
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+        //expression filtre verebilir isterse vermeyedebiler null ile 
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                //eğer filtre verilmemişse tüm veritabanını listele ama verilmişse : eğer filtre verilmişse filtereye göre listele
+                return filter == null ? context.Set<TEntity>().ToList() : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            //IDisposable pattern implementation of c#
+            using (TContext context = new TContext())
+            {
+                //addedEntity : eklenen varlık 
+                //referansı yakala
+                var updatedEntity = context.Entry(entity);
+                //güncelenek bir nesne 
+                updatedEntity.State = EntityState.Modified;
+                //güncelle
+                context.SaveChanges();
+            }
+        }
     }
 }
--- DataAccess/Concrete/EntityFramework/EFRequestDal.cs
+++ DataAccess/Concrete/EntityFramework/EFRequestDal.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -10,68 +11,8 @@ using System.Text;
 namespace DataAccess.Concrete.EntityFramework
 {
     //NuGet-paket kullanımı
-    public class EFRequestDal : IRequestDal
+    public class EfRequestDal : EfEntityRepositoryBase<Request,DonationWebsiteContext>, IRequestDal
     {
-        public void Add(Request entity)
-        {
-            //IDisposable pattern implementation of c#
-            using (DonationWebsiteContext context = new DonationWebsiteContext()) 
-            {
-                //addedEntity : eklenen varlık 
-                //referansı yakala
-                var addedEntity = context.Entry(entity);
-                //eklenecek bir nesne olduğunu belirtiyor 
-                addedEntity.State = EntityState.Added;
-                //ekle
-                context.SaveChanges();
-            }
-        }
-
-        public void Delete(Request entity)
-        {
-            //IDisposable pattern implementation of c#
-            using (DonationWebsiteContext context = new DonationWebsiteContext())
-            {
-                //addedEntity : eklenen varlık 
-                //referansı yakala
-                var deletedEntity = context.Entry(entity);
-                //silinecek  bir nesne 
-                deletedEntity.State = EntityState.Deleted;
-                //sil
-                context.SaveChanges();
-            }
-        }
-        //tek data getirmek için yazıldı
-        public Request Get(Expression<Func<Request, bool>> filter)
-        {
-            using (DonationWebsiteContext context = new DonationWebsiteContext())
-            {
-                return context.Set<Request>().SingleOrDefault(filter);
-            }
-        }
-        //expression filtre verebilir isterse vermeyedebiler null ile 
-        public List<Request> GetAll(Expression<Func<Request, bool>> filter = null)
-        {
-            using (DonationWebsiteContext context = new DonationWebsiteContext())
-            {
-                //eğer filtre verilmemişse tüm veritabanını listele ama verilmişse : eğer filtre verilmişse filtereye göre listele
-                return filter == null ? context.Set<Request>().ToList() : context.Set<Request>().Where(filter).ToList();
-            }
-        }
-
-        public void Update(Request entity)
-        {
-            //IDisposable pattern implementation of c#
-            using (DonationWebsiteContext context = new DonationWebsiteContext())
-            {
-                //addedEntity : eklenen varlık 
-                //referansı yakala
-                var updatedEntity = context.Entry(entity);
-                //güncelenek bir nesne 
-                updatedEntity.State = EntityState.Modified;
-                //güncelle
-                context.SaveChanges();
-            }
-        }
+        
     }
 }
