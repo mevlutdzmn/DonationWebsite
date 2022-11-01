24d5ef2ac041a27b0fc6c723a2b14b2a23fb8ef7
Mevlut Dizman <mdizman124@gmail.com>
2022-11-01 15:57:07 +0300

    mevlut

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DataAccess.EntityFramework
+{
+    //entity tip ve context tip ver
+    class EfEntityRepositoryBase<TEntity,TContext>
+    {
+    }
+}
