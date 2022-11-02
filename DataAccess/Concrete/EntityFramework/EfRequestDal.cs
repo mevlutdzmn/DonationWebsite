7d993682387dca81f72cfe67d6d1419c9f31e357
Mevlut Dizman <mdizman124@gmail.com>
2022-11-02 13:37:56 +0300

    mevlut

--- DataAccess/Concrete/EntityFramework/EfRequestDal.cs
+++ DataAccess/Concrete/EntityFramework/EfRequestDal.cs
@@ -1,6 +1,7 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -11,8 +12,26 @@ using System.Text;
 namespace DataAccess.Concrete.EntityFramework
 {
     //NuGet-paket kullanımı
-    public class EfRequestDal : EfEntityRepositoryBase<Request,DonationWebsiteContext>,IRequestDal
+    public class EfRequestDal : EfEntityRepositoryBase<Request, DonationWebsiteContext>, IRequestDal
     {
-        
+        public List<RequestDetailDto> GetRequestDetails()
+        {
+            using (DonationWebsiteContext context= new DonationWebsiteContext())
+            {
+                //jion işlemi yapıldı
+                var result = from r in context.Requests
+                             join c in context.Categories
+                             on r.CategoryId equals c.CategoryId
+                             select new RequestDetailDto
+                             {
+                                 RequestId = r.RequestId,
+                                 CategoryName = c.CategoryName,
+                                 ReasonRequest = r.ReasonRequest,
+                                 Wallet = r.Wallet,
+                                 CollectedAid = r.CollectedAid
+                             };
+                return result.ToList();
+            }
+        }
     }
 }
