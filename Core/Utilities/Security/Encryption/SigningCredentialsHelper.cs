4de5ac80cc7d3b8bd65a605b56f15c5ffa5fe4e2
Mevlut Dizman <mdizman124@gmail.com>
2022-11-11 19:48:43 +0300

    mevlut

---
+++ Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Encryption
+{
+    public class SigningCredentialsHelper
+    {
+        //hangi anahtar hangi algoritma 
+        public static SigningCredentials CreateSigningCredentials(SecurityKey securityKey)
+        {
+            //securityKey-güvenlik anahtarın bu
+            //SecurityAlgorithms.HmacSha512Signature-şifreleme algoritman budur 
+            return new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512Signature);
+        }
+    }
+}
---
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Hashing
+{
+    public class HashingHelper
+    {
+        // verdiğimiz bir passwordun hash ini oluşturuyor bu method,saltınıda oluşturuyor
+        public static void CreatePasswordHash
+            (string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            //kullanacağımız algoritma
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                //(Encoding.UTF8.GetBytes(password)----> bir stringin byte karşılığını almak için kullanılır
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+
+            }
+        }
+        //passwordhash ı doğrulamak için yazıldı
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                //hesaplanan hashın tüm değerlerini dolaş 
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+
+        }
+    }
+}
